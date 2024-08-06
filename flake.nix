64e6ae6b2352d70296822425f6cb964287d9b3bc
x123 <x123@users.noreply.github.com>
2024-08-06 13:35:46 +0200

    dvt use correct repo

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
                 --experimental-features 'nix-command flakes' \
                 flake init \
                 --template \
-                "github:the-nix-way/dev-templates#''${TEMPLATE}"
+                "github:x123/dev-templates#''${TEMPLATE}"
             '';
             binary-cache = prev.writeScriptBin "binary-cache" ''
               if [ -z $1 ]; then
