820d7f5bbafc7352745352133eba5e17df4914a7
x123 <x123@users.noreply.github.com>
2024-08-06 14:19:50 +0200

    update nix dev-env

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,31 +1,65 @@
 {
   description = "A Nix-flake-based Nix development environment";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            cachix
-            lorri
-            niv
-            nixfmt
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    pre-commit-hooks,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          inherit system;
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        });
+  in {
+    checks = forEachSupportedSystem ({
+      pkgs,
+      system,
+    }: {
+      pre-commit-check = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          alejandra.enable = true;
+          alejandra.settings.check = true;
+          deadnix.enable = true;
+          deadnix.settings = {
+            noLambdaArg = true;
+            noLambdaPatternNames = true;
+          };
+          shellcheck.enable = true;
+          statix.enable = false;
+        };
+      };
+    });
+    devShells = forEachSupportedSystem ({
+      pkgs,
+      system,
+    }: {
+      default = pkgs.mkShell {
+        packages = builtins.attrValues {
+          inherit
+            (pkgs)
+            alejandra
+            deadnix
+            shellcheck
             statix
             vulnix
-            haskellPackages.dhall-nix
-            rnix-lsp
-          ];
-
-          shellHook = ''
-            export PATH="$PWD/bin:$PATH"
-          '';
+            ;
         };
-      });
-    };
+
+        shellHook = ''
+          export PATH="$PWD/bin:$PATH"
+        '';
+      };
+    });
+  };
 }
