b78e9520c9ff43422cb3cc7b5c13e5b552dec655
x123 <x123@users.noreply.github.com>
2024-08-06 15:30:23 +0200

    elixir: update dev env

--- elixir/flake.nix
+++ elixir/flake.nix
@@ -1,36 +1,51 @@
 {
   description = "standard elixir devShell w/ postgres";
 
-  #inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = (with pkgs;
-            [
-              beam.packages.erlangR26.elixir_1_16
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          inherit system;
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        });
+  in {
+    devShells = forEachSupportedSystem ({
+      pkgs,
+      system,
+    }: {
+      default = pkgs.mkShell {
+        packages =
+          builtins.attrValues {
+            inherit
+              (pkgs)
               git
               erlang
               postgresql_16
-            ]) ++
+              ;
+            inherit
+              (pkgs.beam.packages.erlang_26)
+              elixir_1_16
+              ;
+          }
+          ++
           # Linux only
-          pkgs.lib.optionals (pkgs.stdenv.isLinux) (with pkgs; [ inotify-tools libnotify ]) ++
+          pkgs.lib.optionals (pkgs.stdenv.isLinux) (with pkgs; [inotify-tools libnotify])
+          ++
           # macOS only
-          pkgs.lib.optionals (pkgs.stdenv.isDarwin) (with pkgs; [ terminal-notifier ]) ++
-          pkgs.lib.optionals (pkgs.stdenv.isDarwin) (with pkgs.darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
+          pkgs.lib.optionals (pkgs.stdenv.isDarwin) (with pkgs; [terminal-notifier])
+          ++ pkgs.lib.optionals (pkgs.stdenv.isDarwin) (with pkgs.darwin.apple_sdk.frameworks; [CoreFoundation CoreServices]);
 
-          shellHook = ''
-            #export PGDATA="$PWD/db"
-            export PATH="$HOME/.mix/escripts:$PWD/bin:$PATH"
-          '';
-        };
-      });
-    };
+        shellHook = ''
+          #export PGDATA="$PWD/db"
+          export PATH="$HOME/.mix/escripts:$PWD/bin:$PATH"
+        '';
+      };
+    });
+  };
 }
