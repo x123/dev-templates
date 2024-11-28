1643f6b91cc94c2799e69118a1b4735ba651c1e4
x123 <x123@users.noreply.github.com>
2024-11-28 13:31:25 +0100

    update goenv

--- go/flake.nix
+++ go/flake.nix
@@ -1,34 +1,75 @@
 {
-  description = "A Nix-flake-based Go development environment";
+  description = "flake for Golang 1.22 devenv";
 
-  #inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      goVersion = 21; # Change this to update the whole stack
-      overlays = [ (final: prev: { go = prev."go_1_${toString goVersion}"; }) ];
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            # go 1.20 (specified by overlay)
-            go
-
-            # goimports, godoc, etc.
-            gotools
-
-            # https://github.com/golangci/golangci-lint
-            golangci-lint
-          ];
-          shellHook = ''
-          export PATH=~/go/bin:$PATH
-          '';
+  outputs = {
+    self,
+    nixpkgs,
+    pre-commit-hooks,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          inherit system;
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        });
+  in {
+    checks = forEachSupportedSystem ({system, ...}: {
+      pre-commit-check = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          # nix
+          alejandra.enable = true;
+          alejandra.settings.check = true;
+          deadnix.enable = true;
+          deadnix.settings = {
+            noLambdaArg = true;
+            noLambdaPatternNames = true;
+          };
+          flake-checker.enable = true;
+          # golang
+          # revive.enable = true;
+          gofmt.enable = true;
+          gotest.enable = true;
+          # shell scripts
+          shellcheck.enable = true;
+          beautysh.enable = true;
+          # JSON
+          check-json.enable = true;
+          # generic
+          check-toml.enable = true;
         };
-      });
-    };
+      };
+    });
+
+    devShells = forEachSupportedSystem ({
+      pkgs,
+      system,
+    }: {
+      default = pkgs.mkShell {
+        name = "go-dev";
+        shellHook = ''
+          export PATH="$PWD/bin:$PATH"
+          ${self.checks.${system}.pre-commit-check.shellHook}
+        '';
+        packages =
+          builtins.attrValues {
+            inherit
+              (pkgs)
+              go_1_23
+              gotools
+              golangci-lint
+              ;
+          }
+          ++ self.checks.${system}.pre-commit-check.enabledPackages;
+      };
+    });
+  };
 }
